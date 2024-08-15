82d3cc6a69b4ef645ab5617e1ef7f20d9f82bcdc
Nahid <nahidcse1970@gmail.com>
2024-08-15 12:41:05 +0600

    update

--- Reactproject/src/components/Navbar.jsx
+++ Reactproject/src/components/Navbar.jsx
@@ -1,13 +1,10 @@
 import { Suspense, useRef, useState } from "react";
-import { NavLink } from "react-router-dom";
+import { Link, useMatch, useResolvedPath } from "react-router-dom";
 import styled from "styled-components";
 import { useOutsideClicker } from "../utils/helper";
 import { Container } from "./style/Container_styled";
 import { Loader } from "./style/Loader_styled";
-import { StyledNavbar } from "./style/navbar_styled";
 import { Typography } from "./style/Typography_styled";
-import { Flex } from "./style/Flex_styled";
-import { Menu } from "./Menu";
 
 
 
@@ -16,28 +13,6 @@ const NavbarContainer = styled.div`
   background-color:#FFFFFF;
   padding: 5px 5px;
 `;
-
-const NavList = styled.ul`
-  list-style: none;
-  padding: 0;
-  margin: 0;
-  display: flex;
-  align-items: center;
-`;
-
-const NavItem = styled.li`
-  margin-right: 2px;
-`;
-
-const NavLinkStyled = styled(NavLink)`
-  color:#000000;
-  padding: 10px;
-  &:hover {
-    background-color: #555; /* Darker background on hover */
-    color:#000000; 
-  }
-`;
-
 export const Navbar = () => {
   const [subMod, setSubMod] = useState(false);
   const subModRef = useRef(null);
@@ -47,53 +22,42 @@ export const Navbar = () => {
     <Suspense fallback={<Loader />}>
       <NavbarContainer>
         <Container border={"none"}>
-          <StyledNavbar>
-            <Flex row>
-              <Flex md={11}>
-                <NavList>
-                  <NavItem>
-                    <NavLinkStyled to="/">Home</NavLinkStyled>
-                  </NavItem>
-                  <NavItem ref={subModRef}>
-                    <NavLinkStyled to="#" onClick={() => setSubMod(!subMod)}>
-                      Skills
-                    </NavLinkStyled>
-                    {subMod && (
-                      <Menu open={subMod}>
-                        <NavList>
-                          <NavItem><NavLinkStyled to="#skill1">Skill 1</NavLinkStyled></NavItem>
-                          <NavItem><NavLinkStyled to="#skill2">Skill 2</NavLinkStyled></NavItem>
-                        </NavList>
-                      </Menu>
-                    )}
-                  </NavItem>
-                  <NavItem>
-                    <NavLinkStyled to="#about">About</NavLinkStyled>
-                  </NavItem>
-                  <NavItem>
-                    <NavLinkStyled to="#contact">Contact</NavLinkStyled>
-                  </NavItem>
-                </NavList>
-              </Flex>
-              <Flex md={1}>
-                <NavList>
-                  <NavItem>
-                    <NavLinkStyled to="/login">
-                      <Typography
-                        textAlign="left"
-                        color="font"
-                        fontSize="navFont"
-                      >
-                        Login
-                      </Typography>
-                    </NavLinkStyled>
-                  </NavItem>
-                </NavList>
-              </Flex>
-            </Flex>
-          </StyledNavbar>
+          <nav className="nav">
+            <Link to="/" className="site-title">
+              New Site
+            </Link>
+            <ul>
+              <CustomLink to="/">Home</CustomLink>
+              <CustomLink to="/about">About</CustomLink>
+              <CustomLink to="/skill1">Skills</CustomLink>
+              <CustomLink to="/contact">Contact</CustomLink>
+              {/* <Link to="/login">
+                <Typography
+                  textAlign="left"
+                  color="font"
+                  fontSize="navFont"
+                >
+                  Login
+                </Typography>
+              </Link> */}
+            </ul>
+
+          </nav>
         </Container>
       </NavbarContainer>
     </Suspense>
   );
 };
+function CustomLink({ to, children, ...props }) {
+  const resolvedPath = useResolvedPath(to)
+  const isActive = useMatch({ path: resolvedPath.pathname, end: true })
+
+
+  return (
+    <li className={isActive ? "active" : ""}>
+      <Link to={to} {...props}>
+        {children}
+      </Link>
+    </li>
+  )
+}
